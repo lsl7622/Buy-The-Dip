c53a4907c2c5a66ad55e66ce2303b780418859a7
cfrye3 <cjf418@stern.nyu.edu>
2024-08-08 19:38:31 -0400

    email chart LETS GOOO

--- app/emailchart.py
+++ app/emailchart.py
@@ -1,32 +1,38 @@
+# need to pip install -U kaleido (for image chart)
+
 import smtplib
 import os
 from email.mime.text import MIMEText
-from data import ticker, email
-from meta_data import fig
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from app.data import ticker, email, df_five_years
+from app.meta_data import five_yr_candle_stick_chart, ticker_meta_data
+fig1 = five_yr_candle_stick_chart(df_five_years, ticker)
+
 from dotenv import load_dotenv
 load_dotenv()
 
-def send_email_with_chart(to_email, body, fig): #send email function
+def send_email_with_chart(to_email, body, fig1): #send email function
     from_email = os.getenv('YOUR_EMAIL_ADDRESS')
     from_password = os.getenv('YOUR_EMAIL_PASSWORD')
 
-    msg = MIMEText(body) #this is where we set the content
-    msg['Subject'] = f'Stock Alert: {ticker}'
+    msg = MIMEMultipart() #this is where we set the content
+    msg['Subject'] = f'Stock Alert: {ticker} & Candlestick Chart'
     msg['From'] = from_email
     msg['To'] = to_email
 
     # Add text to the email body
 
-    body = f"Here's the candlestick chart for {ticker}:\n\n" 
+    body = f"Here's the candlestick chart for {ticker}:\n\n" # this is where the text in the body of the email appears
     text_part = MIMEText(body, 'plain')
     msg.attach(text_part)
 
     # Save the Plotly chart as an image
-    fig.write_image("chart.png")
+    fig1.write_image("chart.png")
 
     # Attach the chart image to the email
-    with open('chart.png', 'rb') as img_file:
-        img_part = MIMEImage(img_file.read())
+    with open("chart.png", 'rb') as f:
+        img_part = MIMEImage(f.read())
         img_part.add_header('Content-Disposition', 'attachment', filename='chart.png')
         msg.attach(img_part)
 
@@ -38,4 +44,4 @@ def send_email_with_chart(to_email, body, fig): #send email function
     print(f"Email with chart sent to {to_email}")
 
 # Sending the email:
-send_email_with_chart(email, ticker, fig) # need to define the body?
+send_email_with_chart(email, ticker, fig1) # need to define the body?
