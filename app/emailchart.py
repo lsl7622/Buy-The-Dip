efb215b2547990f84ae9c2f8c23d3c4e2ab8458c
lsl7622 <lsl7622@stern.nyu.edu>
2024-08-08 20:20:15 -0400

    Update email chart

--- app/emailchart.py
+++ app/emailchart.py
@@ -7,11 +7,35 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 from app.data import ticker, email, df_five_years
 from app.meta_data import five_yr_candle_stick_chart, ticker_meta_data
+import finnhub 
 ticker_meta_data(ticker)
 fig1 = five_yr_candle_stick_chart(df_five_years, ticker)
 
 from dotenv import load_dotenv
 load_dotenv()
+
+F_API_KEY = os.getenv("FINNHUB_API_KEY")
+# Setup client
+finnhub_client = finnhub.Client(api_key= F_API_KEY) #cqnqotpr01qo8864pu70cqnqotpr01qo8864pu7g (CF added to notebook secrets)
+
+
+def ticker_meta_data(ticker):
+  finance_data = []
+  current_price = finnhub_client.quote(ticker)
+  current_price = current_price['c']
+  print('Current Price','$',current_price)
+  finance_data.append(finnhub_client.company_basic_financials(f'{ticker}', 'all'))
+  meta_data1 = finance_data[0]['metric']['52WeekHigh']
+  meta_data2 = finance_data[0]['metric']['52WeekLow']
+  meta_data3 = finance_data[0]['metric']['52WeekLowDate']
+  meta_data4 = ((meta_data1 - meta_data2)/(meta_data1))*100
+  meta_data5 = finance_data[0]['metric']['beta']
+  # Include Yahoo Finance URL
+  print('See more on Yahoo Finance:',f"https://finance.yahoo.com/quote/{ticker}/") # ticker is the varibale used above
+  return current_price, meta_data1, meta_data2, meta_data3, meta_data4, meta_data5
+# Example usage:
+#ticker_meta_data(f'{ticker}')
+
 
 def send_email_with_chart(to_email, body, fig1): #send email function
     from_email = os.getenv('YOUR_EMAIL_ADDRESS')
@@ -23,11 +47,23 @@ def send_email_with_chart(to_email, body, fig1): #send email function
     msg['To'] = to_email
 
     # Add text to the email body
+    current_price, meta_data1, meta_data2, meta_data3, meta_data4, meta_data5 = ticker_meta_data(ticker)
 
-    body = f"""Here's the candlestick chart for {ticker}:\n\n
-    Current Price: ${current_price}
+    body = f"""
+    Stock Analysis for {ticker}
+
+    Current Price: ${current_price:.2f}  # Format with 2 decimal places
+    52-week high: ${meta_data1:.2f}
+    5-year high: ${meta_data1:.2f}  # Assuming this is the same as 52-week high
+    52-week correction territory: ${meta_data2 * 0.9:.2f}  # 10% below 52-week low
+    5-year correction territory: ${meta_data2 * 0.9:.2f}   # Assuming same as 52-week
+    52 Week High: $ {meta_data1:.2f}
+    52 Week Low & Date: $ {meta_data2:.2f} , {meta_data3}
+    52 Week Percent change: {meta_data4:.2f} %
+    Beta: {meta_data5:.2f}
+    See more on Yahoo Finance: https://finance.yahoo.com/quote/{ticker}/
+    """
     
-    """ # this is where the text in the body of the email appears
     text_part = MIMEText(body, 'plain')
     msg.attach(text_part)
 
