cba404be420d339514de0dda265344bcd6d3e808
lsl7622 <lsl7622@stern.nyu.edu>
2024-08-11 19:29:49 -0400

    update csv file

--- app/localspreadsheet.py
+++ app/localspreadsheet.py
@@ -1,24 +1,30 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+#import gspread
+#from oauth2client.service_account import ServiceAccountCredentials
 
 # Define the scope and credentials (same as before)
-scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('your_credentials_file.json',   
- scope)   
+#scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+#creds = ServiceAccountCredentials.from_json_keyfile_name('your_credentials_file.json',   
+ #scope)   
 
 # Authorize and open the spreadsheet using its ID
-client = gspread.authorize(creds)
-spreadsheet_id = '1TQQbfBP7BziHBfw4tYq0acDb87GXkAmFaXp2ExvK_i8' 
-sheet = client.open_by_key(spreadsheet_id).sheet1  # Replace 'sheet1' with the actual sheet name if needed
+#client = gspread.authorize(creds)
+#spreadsheet_id = '1TQQbfBP7BziHBfw4tYq0acDb87GXkAmFaXp2ExvK_i8' 
+#sheet = client.open_by_key(spreadsheet_id).sheet1  # Replace 'sheet1' with the actual sheet name if needed
 
 # Read the values (assuming they're in the first two cells of the first row)
-value1 = sheet.cell(1, 1).value
-value2 = sheet.cell(1, 2).value
+#value1 = sheet.cell(1, 1).value
+#value2 = sheet.cell(1, 2).value
 
-print(value1, value2)
+#print(value1, value2)
 
-value1 = sheet.cell(2, 3).value
-value2 = sheet.cell(2, 4).value
+#value1 = sheet.cell(2, 3).value
+#value2 = sheet.cell(2, 4).value
 
-value3=365
-#Test
+# CSV Code
+
+import csv
+
+with open('Local_Python_Project_Sheet - Sheet1.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
