735633656d445a50edf0ca6099d91ea403e147f9
cfrye3 <cjf418@stern.nyu.edu>
2024-08-11 20:50:46 -0400

    Clean Up Commit

--- app/localspreadsheet.py
+++
@@ -1,9 +0,0 @@
-# CSV Code
-
-import csv
-
-with open('Local_Python_Project_Sheet - Sheet1.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
-        append.
